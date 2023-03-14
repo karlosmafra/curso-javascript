3895a35ca49f40334965b1c16ec56bd5daf483dd
karlosmafra <kadumaframoraes@gmail.com>
2023-03-13 23:26:44 -0300

    site hora

--- exercicios/site_horario/script.js
+++ exercicios/site_horario/script.js
@@ -1,2 +1,16 @@
+var agora = new Date()
+var hora = agora.getHours()
 var frase = document.querySelector('div#frase')
-frase.innerHTML = "Agora são horas"
+var fundo = document.querySelector('body')
+var foto = document.querySelector('div#foto')
+frase.innerHTML = `Agora são ${hora} horas.`
+
+if (hora < 6) {
+    fundo.style.backgroundColor = '#101010'
+} else if (hora < 12) {
+    fundo.style.backgroundColor = '#ADD8E6'
+} else if (hora < 18) {
+    fundo.style.backgroundColor = '#FFFACD'
+} else {
+    fundo.style.backgroundColor = '#090619'
+}
