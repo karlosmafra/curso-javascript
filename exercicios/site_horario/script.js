0b064aadab48a1a2622179be2d71b745c8d84ca0
karlosmafra <kadumaframoraes@gmail.com>
2023-03-14 20:29:00 -0300

    site idade

--- exercicios/site_horario/script.js
+++ exercicios/site_horario/script.js
@@ -1,16 +1,22 @@
-var agora = new Date()
-var hora = agora.getHours()
-var frase = document.querySelector('div#frase')
-var fundo = document.querySelector('body')
-var foto = document.querySelector('div#foto')
-frase.innerHTML = `Agora são ${hora} horas.`
+function carregar() {
+    var agora = new Date()
+    var hora = agora.getHours()
+    var frase = document.querySelector('div#frase')
+    var fundo = document.querySelector('body')
+    var img = document.querySelector('img#imagem')
+    frase.innerHTML = `Agora são ${hora} horas.`
 
-if (hora < 6) {
-    fundo.style.backgroundColor = '#101010'
-} else if (hora < 12) {
-    fundo.style.backgroundColor = '#ADD8E6'
-} else if (hora < 18) {
-    fundo.style.backgroundColor = '#FFFACD'
-} else {
-    fundo.style.backgroundColor = '#090619'
+    if (hora < 6) {
+        fundo.style.backgroundColor = '#101010'
+        img.src = 'imagens/dawn.png'
+    } else if (hora < 12) {
+        fundo.style.backgroundColor = '#ADD8E6'
+        img.src = 'imagens/morning.png'
+    } else if (hora < 18) {
+        fundo.style.backgroundColor = '#eeac8f'
+        img.src = 'imagens/afternoon.png'
+    } else {
+        fundo.style.backgroundColor = '#090619'
+        img.src = 'imagens/night.png'
+    }
 }
--- exercicios/site_idade/script.js
+++ exercicios/site_idade/script.js
@@ -1,7 +1,44 @@
-var data = document.querySelector('input#data')
-var ano = Number(data.value)
-var hoje = new Date()
-var atual = hoje.getFullYear()
 function verificar() {
-    alert(atual)
+    var data = document.querySelector('input#data')
+    var ano = Number(data.value)
+    var hoje = new Date()
+    var atual = hoje.getFullYear()
+    var res = document.querySelector('div#res')
+    if (ano > atual || ano <= 0) {
+        alert('[ERRO]: Ano de nascimento inválido.')
+    } else {
+        var idade = atual - ano
+        var sexo = document.getElementsByName('sexo')
+        var gen = ''
+        var img = document.createElement('img')
+        img.setAttribute('id', 'foto')
+        if (sexo[0].checked) {
+            gen = 'um homem'
+            if (idade < 12) {
+                img.setAttribute('src', 'imagens/vincent.png')
+            } else if (idade < 21) {
+                img.setAttribute('src', 'imagens/sam.png')
+            } else if (idade < 60) {
+                img.setAttribute('src', 'imagens/demetrius.png')
+            } else {
+                img.setAttribute('src', 'imagens/george.png')
+            }
+        } else {
+            gen = 'uma mulher'
+            if (idade < 12) {
+                img.setAttribute('src', 'imagens/jas.png')
+            } else if (idade < 21) {
+                img.setAttribute('src', 'imagens/abigail.png')
+            } else if (idade < 60) {
+                img.setAttribute('src', 'imagens/caroline.png')
+            } else {
+                img.setAttribute('src', 'imagens/evelyn.png')
+            }
+        }
+        
+        res.innerHTML = `Você é ${gen} de ${idade} anos.`
+        res.appendChild(img)
+    }
 }
+
+9769
